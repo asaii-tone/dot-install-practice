1a227f730c3be8217f993fc3a92bfaaf528e1f84
asaii-tone <lovelove.poem.sing@gmail.com>
2021-08-13 14:37:51 +0900

    JS_DOM講座完了

---
+++ MyJS/dom/js/main.js
@@ -0,0 +1,110 @@
+"use strict";
+
+{
+  // function update() {
+  //   // document.querySelector('h1').textContent = 'Changed!';
+  //   // document.querySelector('#target').textContent = 'Changed!';
+  //   // document.getElementById('target').textContent = 'Changed!';
+  //   // document.querySelectorAll('p').forEach((p, index) =>{
+  //   //   p.textContent=`${index}番目のpです。`
+  //   // });
+  // }
+  // document.querySelector("button").addEventListener("click", () => {
+  //   const targetNode = document.getElementById("target");
+
+  //   document.querySelectorAll("p").forEach((p, index) => {
+  //     p.textContent = `${index}番目のpです。`;
+  //   });
+  //   targetNode.title = "this is title";
+  //   targetNode.style.color = "red";
+  // });
+
+  // setTimeout(update, 1000);
+
+  // document.querySelector("button").addEventListener("click", () => {
+  //   const targetNode = document.getElementById("target");
+
+  //   // targetNode.className = 'my-color';
+  //   // targetNode.className = 'my-color my-border';
+  //   // targetNode.classList.add('my-color');
+  //   // if (targetNode.classList.contains('my-color') === true) {
+  //   //   targetNode.classList.remove('my-color');
+  //   // } else {
+  //   //   targetNode.classList.add('my-color');
+  //   // }
+  //   targetNode.classList.toggle("my-color");
+  // });
+
+  document.querySelector("button").addEventListener("click", () => {
+    // const targetNode = document.getElementById("target");
+
+    // // targetNode.textContent = "Dotinstall";
+    // targetNode.textContent = targetNode.dataset.translation;
+
+    // const item2 = document.createElement('li');
+    // item2.textContent = 'item 2'
+    // const ul = document.querySelector('ul');
+    // ul.appendChild(item2);
+
+    // const item0 = document.querySelectorAll("li")[0];
+    // const copy = item0.cloneNode(true);
+    // const ul = document.querySelector("ul");
+    // const item2 = document.querySelectorAll("li")[2];
+    // ul.insertBefore(copy, item2);
+
+    // const item1 = document.querySelectorAll("li")[1];
+    // // item1.remove();
+    // // 親Node.removeChild(削除するNode)
+    // document.querySelector('ul').removeChild(item1);
+
+    // const li = document.createElement("li");
+    // const text = document.querySelector("input");
+    // li.textContent = text.value;
+    // document.querySelector("ul").appendChild(li);
+    // text.value = "";
+    // text.focus();
+
+    // const li = document.createElement("li");
+    // const color = document.querySelector("select");
+    // li.textContent = `${color.value} - ${color.selectedIndex}`;
+    // document.querySelector("ul").appendChild(li);
+
+    // const colors = document.querySelectorAll("input");
+    // let selectedColor;
+    // colors.forEach((color) => {
+    //   if (color.checked === true) {
+    //     selectedColor = color.value;
+    //   }
+    // });
+    // const li = document.createElement("li");
+    // li.textContent = `${selectedColor}`;
+    // document.querySelector("ul").appendChild(li);
+
+    // const colors = document.querySelectorAll("input");
+    // let selectedColors = [];
+    // colors.forEach((color) => {
+    //   if (color.checked === true) {
+    //     selectedColors.push(color.value);
+    //   }
+    // });
+    // const li = document.createElement("li");
+    // li.textContent = selectedColors;
+    // document.querySelector("ul").appendChild(li);
+
+    const text = document.querySelector('textarea');
+
+  // text.addEventListener('focus', () => {
+  //   console.log('focus');
+  // });
+  // text.addEventListener('blur', () => {
+  //   console.log('blur');
+  // });
+  // text.addEventListener('input', () => {
+  //   // console.log('input');
+  //   console.log(text.value.length);
+  // });
+  // text.addEventListener('change', () => {
+  //   console.log('change');
+  // });
+  });
+}
