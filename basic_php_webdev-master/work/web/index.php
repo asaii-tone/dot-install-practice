8e00cc81c515c86d3ee1f5e5246f125dce6307d4
asaii-tone <lovelove.poem.sing@gmail.com>
2022-03-17 00:18:05 +0900

    ✨ Cookieの学習など

--- basic_php_webdev-master/work/web/index.php
+++ basic_php_webdev-master/work/web/index.php
@@ -4,22 +4,25 @@ require('../app/functions.php');
 
 include('../app/_parts/_header.php');
 
-$names = [
-  'taro',
-  'jiro',
-  'saburo',
-];
-
 ?>
-  <ul>
-    <?php if (empty($names)): ?>
-      <li>Nobody!</li>
-    <?php else: ?>
-      <?php foreach ($names as $name): ?>
-        <li>Hello, <?= h($name); ?>!</li>
-      <?php endforeach; ?>
-    <?php endif; ?>
-  </ul>
+
+<form action="result.php" method="get">
+  <label><input type="radio" name="color" value="orange">Orange</label>
+  <label><input type="radio" name="color" value="pink">Pink</label>
+  <label><input type="radio" name="color" value="gold">Gold</label>
+  <!-- <label><input type="checkbox" name="colors[]" value="orange">Orange</label>
+  <label><input type="checkbox" name="colors[]" value="pink">Pink</label>
+  <label><input type="checkbox" name="colors[]" value="gold">Gold</label> -->
+  <!-- <select name="colors[]" multiple>
+    <option value="orange">Orange</option>
+    <option value="pink">Pink</option>
+    <option value="gold">Gold</option>
+  </select> -->
+  <!-- <textarea name="message"></textarea> -->
+  <!-- <input type="text" name="username"> -->
+  <button>Send</button>
+  <a href="reset.php">reset</a>
+</form>
 
 <?php 
 
---
+++ basic_php_webdev-master/work/web/result.php
@@ -0,0 +1,29 @@
+<?php
+
+  require('../app/functions.php');
+
+  // $message = trim(filter_input(INPUT_GET, 'message'));
+  // $message = $message !== '' ? $message : '...';
+  // $username = filter_input(INPUT_GET, 'username');
+
+  // $colors = filter_input(INPUT_GET, 'colors', FILTER_DEFAULT, FILTER_REQUIRE_ARRAY);
+  // $colors = empty($colors) ? 'None selected' : implode(',', $colors);
+  
+  // $color = filter_input(INPUT_GET, 'color');
+  // $color = isset($color) ? $color : 'None selected';
+  // $color = $color ?? 'None selected';
+  $colorFromGet = filter_input(INPUT_GET, 'color') ?? 'transparent';
+  setcookie('color', $colorFromGet);
+
+
+  include('../app/_parts/_header.php');
+
+?>
+
+<!-- <p><?= nl2br(h($message)); ?> by <?= h($username); ?></p> -->
+<p><?= h($colorFromGet); ?></p>
+<p><a href="index.php">Go Back</a></p>
+
+<?php
+  include('../app/_parts/_footer.php');
+?>
