532c30c802a03c9d86be0c1240fb284bc6febf95
asaii-tone <lovelove.poem.sing@gmail.com>
2022-03-19 09:50:43 +0900

    POST学習準備

--- basic_php_webdev-master/work/web/index.php
+++ basic_php_webdev-master/work/web/index.php
@@ -5,23 +5,8 @@ require('../app/functions.php');
 include('../app/_parts/_header.php');
 
 ?>
-
-<form action="result.php" method="get">
-  <label><input type="radio" name="color" value="orange">Orange</label>
-  <label><input type="radio" name="color" value="pink">Pink</label>
-  <label><input type="radio" name="color" value="gold">Gold</label>
-  <!-- <label><input type="checkbox" name="colors[]" value="orange">Orange</label>
-  <label><input type="checkbox" name="colors[]" value="pink">Pink</label>
-  <label><input type="checkbox" name="colors[]" value="gold">Gold</label> -->
-  <!-- <select name="colors[]" multiple>
-    <option value="orange">Orange</option>
-    <option value="pink">Pink</option>
-    <option value="gold">Gold</option>
-  </select> -->
-  <!-- <textarea name="message"></textarea> -->
-  <!-- <input type="text" name="username"> -->
-  <button>Send</button>
-  <a href="reset.php">reset</a>
+<form action="result.php" method="post">
+  <button>Post</button>
 </form>
 
 <?php 
--- basic_php_webdev-master/work/web/result.php
+++ basic_php_webdev-master/work/web/result.php
@@ -2,25 +2,10 @@
 
   require('../app/functions.php');
 
-  // $message = trim(filter_input(INPUT_GET, 'message'));
-  // $message = $message !== '' ? $message : '...';
-  // $username = filter_input(INPUT_GET, 'username');
-
-  // $colors = filter_input(INPUT_GET, 'colors', FILTER_DEFAULT, FILTER_REQUIRE_ARRAY);
-  // $colors = empty($colors) ? 'None selected' : implode(',', $colors);
-  
-  // $color = filter_input(INPUT_GET, 'color');
-  // $color = isset($color) ? $color : 'None selected';
-  // $color = $color ?? 'None selected';
-  $colorFromGet = filter_input(INPUT_GET, 'color') ?? 'transparent';
-  $_SESSION['color'] = $colorFromGet;
-  
   include('../app/_parts/_header.php');
 
 ?>
 
-<!-- <p><?= nl2br(h($message)); ?> by <?= h($username); ?></p> -->
-<p><?= h($colorFromGet); ?></p>
 <p><a href="index.php">Go Back</a></p>
 
 <?php
