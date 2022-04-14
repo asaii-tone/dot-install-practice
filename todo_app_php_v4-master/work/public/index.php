8307c03639d85dd5eb93ebfc6fdda0655ac19f3e
asaii-tone <lovelove.poem.sing@gmail.com>
2022-04-14 10:53:55 +0900

    ✨ create project PHP Todo application

---
+++ todo_app_php_v4-master/work/public/index.php
@@ -0,0 +1,49 @@
+<?php
+
+require_once(__DIR__ . '/../app/config.php');
+
+createToken();
+$pdo = getPdoInstance();
+
+$todos = getTodos($pdo);
+if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+  validateToken();
+  addTodo($pdo);
+  header('Location:' . SITE_URL);
+  exit;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>My Todos</title>
+  <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+  <h1>Todos</h1>
+
+  <form action="" method="post">
+    <input type="text" name="title"  placeholder="add new text .">
+    <input type="hidden" name="token" value="<?= h($_SESSION['token']); ?>">
+  </form>
+  <ul>
+    <?php foreach($todos as $todo): ?>
+      <li>
+        <input type="checkbox" <?= $todo->is_done ? 'checked' : ''; ?>>
+        <span class="<?= $todo->is_done ? 'done' : ''; ?>"><?=h($todo->title); ?></span>
+      </li>
+    <?php endforeach; ?>   
+    <!-- <li>
+      <input type="checkbox" checked>
+      <span class="done">Title</span>
+    </li>
+    <li>
+      <input type="checkbox">
+      <span>Title</span>
+    </li> -->
+  </ul>
+</body>
+</html>
