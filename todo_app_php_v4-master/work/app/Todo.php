dfc5f520488a5ba24868d34de191b6f595c2380a
asaii-tone <lovelove.poem.sing@gmail.com>
2022-04-18 13:24:31 +0900

    ⚡️ JSでのチェックボックスの非同期処理追加

--- todo_app_php_v4-master/work/app/Todo.php
+++ todo_app_php_v4-master/work/app/Todo.php
@@ -25,10 +25,12 @@ class Todo {
         case 'delete':
           $this->delete();
           break;
+        case 'purge':
+          $this->purge();
+          break;
         default:
           exit;
       }
-      header('Location:' . SITE_URL);
       exit;
     }
   }
@@ -61,6 +63,10 @@ class Todo {
     $stmt = $this->pdo->prepare("DELETE FROM todos WHERE id = :id");
     $stmt->bindValue('id', $id, \PDO::PARAM_INT);
     $stmt->execute();
+  }
+
+  private function purge(){
+    $this->pdo->query("DELETE FROM todos WHERE is_done = 1");
   }
 
   public function getAll() {
--- todo_app_php_v4-master/work/public/js/main.js
+++ todo_app_php_v4-master/work/public/js/main.js
@@ -1,10 +1,20 @@
-"use strict";
+'use strict';
 
 {
   const checkboxes = document.querySelectorAll('input[type="checkbox"]');
-  checkboxes.forEach((checkbox) => {
-    checkbox.addEventListener("change", () => {
-      checkbox.parentNode.submit();
+  checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('change', () => {
+      fetch('?action=toggle', {
+        method: 'POST',
+        body: new URLSearchParams({
+          // カスタム属性data-*の*(id, token)を受け取る。
+          id: checkbox.dataset.id,
+          token: checkbox.dataset.token,
+        }),
+      });
+      // データベースが更新されると打ち消し線をつけて、画面も更新されたように見せる。
+      // 非同期処理
+      checkbox.nextElementSibling.classList.toggle('done');
     });
   });
 
@@ -16,5 +26,13 @@
       }
       deleteTodo.parentNode.submit();
     });
+  });
+
+  const purge = document.querySelector(".purge");
+  purge.addEventListener("click", () => {
+    if (!confirm('Are you sure?')){
+      return;
+    }
+    purge.parentNode.submit();
   });
 }
