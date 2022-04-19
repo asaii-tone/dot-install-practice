7635e7400ff0de0cc6d84281d877bdc44f3d08d7
asaii-tone <lovelove.poem.sing@gmail.com>
2022-04-19 21:59:30 +0900

    ✨ 基本文法編完了

--- MyBBS/routes/web.php
+++ MyBBS/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PostController::class, 'index'])
+    ->name('posts.index');
+Route::get('/posts/{id}', [PostController::class, 'show'])
+    ->name('posts.show');
